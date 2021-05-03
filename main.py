7e73d1492f3a5e681d7902843dbf9dd122fdd332
cimenwd <cimenwd@gmail.com>
2021-05-03 23:55:20 +0300

    Bitti

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import BilsemMath
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    c = BilsemMath.Temel.topla (2, 3, 4, 5)
+    print (c)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
