398e6294a254f3f0702b65f188b2be748df72cfe
cimenwd <cimenwd@gmail.com>
2021-05-04 00:09:53 +0300

    Son değişiklik

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import BilsemMath
 
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     c = BilsemMath.Temel.topla (2, 3, 4, 5)
     print (c)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
