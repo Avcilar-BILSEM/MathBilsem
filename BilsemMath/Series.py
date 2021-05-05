78d39befdefcbe8892568d9c5281a307bf4962b6
cimenwd <cimenwd@gmail.com>
2021-05-06 00:06:23 +0300

    Fibonacci eklendi

--- BilsemMath/Series.py
+++ BilsemMath/Series.py
@@ -1,13 +1,15 @@
 from .Temel import Temel
-class Series:
+class Series(Temel):
 
     def __init__(self):
         self.fibb = [0,1]
+        self.teksayilar=[1]
+        self.ciftsayilar = [2]
 
     def fibonacci(self,n):
         if len(self.fibb)<n:
             for i in range(len(self.fibb),n+1):
-                self.fibb.append(self.fibb[i-2]+self.fibb[i-1])
+                self.fibb.append(self.topla(self.fibb[i-2],self.fibb[i-1]))
         return self.fibb[0:n]
 
     def goldenRatio(self,n):
@@ -15,5 +17,16 @@ class Series:
             self.fibonacci(n)
         return self.fibb[n]/self.fibb[n-1]
 
+    def tek(self,n):
+        if n>self.teksayilar[-1]:
+            for i in range(self.teksayilar[-1]+2,n+1,2):
+                self.teksayilar.append(i)
+        return self.teksayilar[0:n]
+
+    def cift(self,n):
+        if n>self.ciftsayilar[-1]:
+            for i in range(self.ciftsayilar[-1]+2,n+1,2):
+                self.ciftsayilar.append(i)
+        return self.ciftsayilar[0:n]
 
 
