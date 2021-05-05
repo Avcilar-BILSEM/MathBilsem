66df27357739a4e067f9add40b53ddf6fc470997
cimenwd <cimenwd@gmail.com>
2021-05-05 15:06:34 +0300

    Fibonacci eklendi

--- BilsemMath/Averages.py
+++ BilsemMath/Averages.py
@@ -1,7 +1,10 @@
 class Averages:
-
+    """Ertuğrul bu sınıfı yaz"""
     def __init__(self):
         pass
 
     def Standart(self,*args):
-        """Ertuğrul bu sınıfı yaz"""
+        if isinstance(args[0],(int,float)):
+            return sum(args)/len(args)
+        else:
+            return sum(args[0])/len(args[0])
