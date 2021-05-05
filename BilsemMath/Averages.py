07d809a805b0cb9c865675c0132eaeb58b6f9ee8
cimenwd <cimenwd@gmail.com>
2021-05-05 15:05:04 +0300

    Fibonacci eklendi

---
+++ BilsemMath/Averages.py
@@ -0,0 +1,7 @@
+class Averages:
+
+    def __init__(self):
+        pass
+
+    def Standart(self,*args):
+        """Ertuğrul bu sınıfı yaz"""
--- BilsemMath/__init__.py
+++ BilsemMath/__init__.py
@@ -1,5 +1,7 @@
 from .Temel import Temel
 from .Series import Series
+from .Averages import Averages
 
 Temel=Temel()
 Series=Series()
+Averages=Averages()
