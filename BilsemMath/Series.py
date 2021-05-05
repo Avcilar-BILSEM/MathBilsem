88391ce8c6a1459b9e162f6646aa45556bdb45b9
cimenwd <cimenwd@gmail.com>
2021-05-05 14:53:23 +0300

    Fibonacci eklendi

---
+++ BilsemMath/Series.py
@@ -0,0 +1,21 @@
+
+class Series:
+
+    def __init__(self):
+        self.fibb = [0,1]
+
+    def fibonacci(self,n):
+        if len(self.fibb)<n:
+            for i in range(len(self.fibb),n+1):
+
+                self.fibb.append(self.fibb[i-2]+self.fibb[i-1])
+        return self.fibb[0:n]
+
+    def goldenRatio(self,n):
+        if len(self.fibb)<n:
+            self.fibonacci(n)
+
+        return self.fibb[n]/self.fibb[n-1]
+
+
+
--- BilsemMath/__init__.py
+++ BilsemMath/__init__.py
@@ -1,3 +1,5 @@
 from .Temel import Temel
+from .Series import Series
 
 Temel=Temel()
+Series=Series()
